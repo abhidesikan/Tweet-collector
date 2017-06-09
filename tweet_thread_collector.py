b6aae22c2f4e11c997b09c8765709ed5c8659767
Abhishek Desikan <abhidesikan@ambp.attlocal.net>
2017-06-08 22:24:43 -0400

    Authenticate using secret key and token

--- tweet_thread_collector.py
+++ tweet_thread_collector.py
@@ -1,8 +1,38 @@
 #!/usr/bin/python
 import twitter
+import configparser
+
+class tweet_auth(object):
+    def __init__(self):
+        self._config = None
+
+    def get_consumer_key(self):
+        return self._get_option('CONSUMER_KEY')
+
+    def get_consumer_secret(self):
+        return self._get_option('CONSUMER_SECRET')
+
+    def get_access_key(self):
+        return self._get_option('ACCESS_KEY_TOKEN')
+
+    def get_access_secret(self):
+        return self._get_option('ACCESS_TOKEN_SECRET')
+
+    def _get_option(self, option):   
+        try:
+            return self._get_config().get('Tweet', option)
+        except:
+            return None
+
+    def _get_config(self):
+        if not self._config:
+            self._config = configparser.ConfigParser()
+            self._config.read('.auth_tweet')
+        return self._config
 
 def authenticate():
-    api = twitter.Api(consumer_key=CONSUMER_KEY,consumer_secret=CONSUMER_SECRET,access_token_key=ACCESS_KEY_TOKEN,access_token_secret=ACCESS_TOKEN_SECRET)
+    auth = tweet_auth()
+    api = twitter.Api(consumer_key=auth.get_consumer_key(),consumer_secret=auth.get_consumer_secret(),access_token_key=auth.get_access_key(),access_token_secret=auth.get_access_secret())
     return api
 
 def get_public_status(user_id, api):
